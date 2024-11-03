ed986f08916209a9c20a2068432bff0956523380
fflores <ffloresrutti@gmail.com>
2024-11-02 23:28:57 -0500

    mod: small

--- FinalProjectAlgorithm/GameObjects.h
+++ FinalProjectAlgorithm/GameObjects.h
@@ -15,16 +15,131 @@ struct Player
 	{
 		for (int i = 0; i < 5; ++i)
 		{
-			for (int j = 0; j < 4; ++j)
+			for (int j = 0; j < 3; ++j)
 			{
 				this->sprite[i][j] = sprite[i][j];
 			}
 		}
 	}
+
+	void move(char input_direction)
+	{
+		if (input_direction == 'w')
+		{
+			position.y--;
+		}
+		else if (input_direction == 's')
+		{
+			position.y++;
+		}
+		else if (input_direction == 'a')
+		{
+			position.x--;
+		}
+		else if (input_direction == 'd')
+		{
+			position.x++;
+		}
+	}
+
+	void draw()
+	{
+		for (int y = 0; y < 5; y++)
+		{
+			for (int x = 0; x < 3; x++)
+			{
+				Console::SetCursorPosition(position.x + x, position.y + y);
+				cout << sprite[y][x];
+			}
+		}
+	}
+
+	void clear()
+	{
+		for (int y = 0; y < 5; y++)
+		{
+			for (int x = 0; x < 3; x++)
+			{
+				Console::SetCursorPosition(position.x + x, position.y + y);
+				cout << " ";
+			}
+		}
+	}
+
+	void update()
+	{
+		clear();
+
+		if (_kbhit())
+		{
+			char input = _getch();
+			move(input);
+		}
+
+		draw();
+	}
 };
 
-struct Enemy
+struct Ally
 {
 	int health;
 	Vector2 position;
+};
+
+struct TrafficLight
+{
+	Vector2 position;
+};
+
+struct Car
+{
+	int health;
+	Vector2 position;
+};
+
+struct Map
+{
+	int width, height;
+	char** map;
+
+	Map(int width, int height)
+		: width(width), height(height)
+	{
+		map = new char* [height];
+		for (int i = 0; i < height; ++i)
+		{
+			map[i] = new char[width];
+		}
+	}
+
+	void draw()
+	{
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				Console::SetCursorPosition(x, y);
+				cout << map[y][x];
+			}
+		}
+	}
+};
+
+struct Game
+{
+	Player* player;
+	Ally* allies;
+	TrafficLight* traffic_lights;
+	Car* cars;
+	Map* map;
+
+	Game(Player* player, Ally* allies, TrafficLight* traffic_lights, Car* cars, Map* map)
+		: player(player), allies(allies), traffic_lights(traffic_lights), cars(cars), map(map)
+	{
+	}
+
+	void update()
+	{
+		player->update();
+	}
 };
