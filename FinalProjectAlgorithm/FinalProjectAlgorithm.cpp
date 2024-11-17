5bf761dc3f711d8d0c4839a909df72909e4f876e
atahualpaxd <victortadeo207@gmail.com>
2024-11-17 09:48:42 -0500

    cars position

--- FinalProjectAlgorithm/FinalProjectAlgorithm.cpp
+++ FinalProjectAlgorithm/FinalProjectAlgorithm.cpp
@@ -15,9 +15,24 @@ int main()
 
 	Player* player = new Player({ 0, 0, 0 }, {70,20}, 10, spr_player, map->map);
 
-	Car* car_01 = new Car(spr_car_01_left, 9, 3, {80, 4}, {255, 0, 0}, map->map, {0, 1}, 0.15f);
-	Car* car_02 = new Car(spr_car_01_left, 9, 3, {70, 4}, { 0, 255, 0 }, map->map, { 0, 1 }, 0.15f);
-	Car* car_03 = new Car(spr_car_01_up, 5, 4, {60, 4}, { 0, 0, 255 }, map->map, { 0, 1 }, 0.15f);
+	Car* car_01 = new Car(spr_car_01_left, 9, 3, {149, 18}, {255, 0, 0}, map->map, {-1, 1}, 0.15f);
+	/*Car* car_02 = new Car(spr_car_02_left, 9, 3, {149, 21}, {255, 0, 0}, map->map, {-1, 1}, 0.15f);
+	Car* car_03 = new Car(spr_car_03_left, 9, 3, {149, 24}, {255, 0, 0}, map->map, {-1, 1}, 0.15f);
+	Car* car_04 = new Car(spr_car_04_left, 9, 3, {149, 27}, {255, 0, 0}, map->map, {-1, 1}, 0.15f);*/
+
+	Car* car_02 = new Car(spr_car_01_right, 9, 3, {2, 18}, { 0, 255, 0 }, map->map, { 1, -1 }, 0.15f);
+	/*Car* car_06 = new Car(spr_car_02_right, 9, 3, {2, 21}, { 0, 255, 0 }, map->map, { 1, -1 }, 0.15f);
+	Car* car_07 = new Car(spr_car_03_right, 9, 3, {2, 24}, { 0, 255, 0 }, map->map, { 1, -1 }, 0.15f);
+	Car* car_08 = new Car(spr_car_04_right, 9, 3, {2, 27}, { 0, 255, 0 }, map->map, { 1, -1 }, 0.15f);*/
+
+	Car* car_03 = new Car(spr_car_01_up, 5, 4, {81, 45}, { 0, 0, 255 }, map->map, { 0, 1 }, 0.15f);
+	/*Car* car_10 = new Car(spr_car_02_up, 5, 4, {75, 45}, { 0, 0, 255 }, map->map, { 0, 1 }, 0.15f);
+	Car* car_11 = new Car(spr_car_03_up, 5, 4, {81, 45}, { 0, 0, 255 }, map->map, { 0, 1 }, 0.15f);
+	Car* car_12 = new Car(spr_car_04_up, 5, 4, {81, 45}, { 0, 0, 255 }, map->map, { 0, 1 }, 0.15f);*/
+
+	Car* car_04 = new Car(spr_car_02_down, 5, 4, {81, 1}, { 0, 0, 0 }, map->map, { 0, -1 }, 0.15f);
+	Car* car_05 = new Car(spr_car_03_down, 5, 4, {74, 1}, { 0, 0, 0 }, map->map, { 0, -1 }, 0.15f);
+
 	/*Car* car_02 = new Car(spr_car_02_left, ConsoleColor::Green, map->map);
 	Car* car_03 = new Car(spr_car_03_left, ConsoleColor::Blue, map->map);
 	Car* car_04 = new Car(spr_car_04_left, ConsoleColor::Yellow, map->map);
@@ -32,7 +47,7 @@ int main()
 	TrafficLight* traffic_lights = new TrafficLight();
 
 	//Car* cars[5] = { car_01, car_02, car_03, car_04, car_05 };
-	Car* cars[5] = { car_01, car_02 , car_03 , car_01 , car_01 };
+	Car* cars[5] = { car_01, car_02 , car_03 , car_04 , car_05, };
 	Ally* allies[5] = { ally_01, ally_02, ally_03, ally_04, ally_05 };
 
 	Game* game = new Game(player, allies, traffic_lights, cars, map);
